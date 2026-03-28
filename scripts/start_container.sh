4d2717fcd8c0c962a0f17c1990834be5fea2d5ec
iamgagana <72552175+iamgagana@users.noreply.github.com>
2026-03-28 20:24:47 +0530

    Change port mapping from 5000 to 5001

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -10,5 +10,5 @@ sudo docker pull gagana13/simple-python-flask-app:latest
 # Run new container
 sudo docker run -d \
   --name simple-python-flask-app \
-  -p 5000:5000 \
+  -p 5001:5001 \
   gagana13/simple-python-flask-app:latest
